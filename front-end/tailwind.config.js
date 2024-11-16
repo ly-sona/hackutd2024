19b8cbf71b9e6ca2bb519dee90f46eb6059dbd65
anu_boyapati <anu.d.boyapati@gmail.com>
2024-11-16 14:52:44 -0600

    front-end edits

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,9 +1,27 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#009ba0",
+          "secondary": "#14b8a6",
+          "accent": "#00a09e",
+          "neutral": "#065f46",
+          "base-100": "#1d2c2f",
+          "info": "#bae6fd",
+          "success": "#67da39",
+          "warning": "#fde047",
+          "error": "#ef4444",
+        },
+      },
+    ],
+  },
+};
