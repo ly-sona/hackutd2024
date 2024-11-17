025ea0558ac87548922d4b928b6bc0ab4aaee5f0
Tejas Ramanujam <101826696+TejasRamanujam@users.noreply.github.com>
2024-11-17 16:04:44 -0600

    merging again

--- back-end/database.py
+++
@@ -1,9 +0,0 @@
-# database.py
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-DATABASE_URL = "postgresql://user:password@localhost/insurewise"  # Update with actual credentials
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
