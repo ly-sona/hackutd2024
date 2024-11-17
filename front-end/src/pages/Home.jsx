13cd9e7ab7b6db5ff438285f24dac8eec1fdf501
anu_boyapati <anu.d.boyapati@gmail.com>
2024-11-17 10:08:33 -0600

    front-end updates

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -6,9 +6,9 @@ export default function Home() {
         <div className="bg-#1a1a2e min-h-screen">
             <Spline  scene="https://prod.spline.design/xWqLUac-cZefUYWv/scene.splinecode" style={{ marginTop: "130px" }} />
             <h1
-  className="text-center text-[5rem] flex items-center justify-center min-h-screen bg-gradient-to-b from-yellow-400 to-yellow-600 text-transparent bg-clip-text drop-shadow-[4px_4px_4px_rgba(0,0,0,0.7)] font-bold" style={{ marginTop: "-1000px" }}
+  className="text-center text-[5rem] flex items-center justify-center min-h-screen bg-gradient-to-b from-violet-100 to-violet-300 text-transparent bg-clip-text drop-shadow-[4px_4px_4px_rgba(0,0,0,0.7)] font-bold" style={{ marginTop: "-1000px" }}
 >
-  Everyone Deserves the Truth
+  Smart Loans, Smart Choices
 </h1>
 
         </div>
