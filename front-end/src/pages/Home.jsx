0ad63a161810e5a409f82b588310c69bf787b791
ch1kim0n1 <chikimoni61@gmail.com>
2024-11-17 09:29:20 -0600

    home text color

--- front-end/src/pages/Home.jsx
+++ front-end/src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
         <div className="bg-#1a1a2e min-h-screen">
             <Spline  scene="https://prod.spline.design/xWqLUac-cZefUYWv/scene.splinecode" style={{ marginTop: "130px" }} />
             <h1
-  className="text-center text-[5rem] font-normal flex items-center justify-center min-h-screen bg-gradient-to-b from-yellow-400 to-yellow-600 text-transparent bg-clip-text drop-shadow-[4px_4px_4px_rgba(0,0,0,0.7)]"
+  className="text-center text-[5rem] font-normal flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-100 to-gray-400 text-transparent bg-clip-text drop-shadow-[4px_4px_4px_rgba(0,0,0,0.7)]"
   style={{ marginTop: "-1000px" }}
 >
   Everyone Deserves the Truth
